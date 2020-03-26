8b2662c964d5477f02e13198fdd634550bf5395d
Bruno Felipe <oliveiraf.120@gmail.com>
2020-03-26 15:19:47 -0400

    Mobile da aplicação

---
+++ mobile/src/pages/utils/functions.js
@@ -0,0 +1,8 @@
+function turnsValueIntoCurrencyBRL(value) {
+  return Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  }).format(value);
+}
+
+export { turnsValueIntoCurrencyBRL };
